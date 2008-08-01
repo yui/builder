4e468534b1e85c7a508a0de4cdec284e4ce24dcf
Satyen Desai <sdesai@yahoo-inc.com>
2008-08-01 20:10:14 +0000

    New Window Required modules now load in the same environment as other example flavors, providing PHP variable/fn access and switching from build to yui.yahooapis paths. Updated the 4 current new window examples we have to use the variables - assetsDirectory, buildDirectory, yuiConfig

--- projects/yui/genexamples3.php
+++ projects/yui/genexamples3.php
@@ -320,7 +320,9 @@ function generateExamples($modules, $examples) {
 	
 						// Requires New Window (XXX_source.html) - Dist: build path, YDN: yui.yahooapis
 						if ($example["newWindow"] == "require") {
-							generateExampleFile("examples/data/$moduleKey/$exampleKey"."_source.php",
+//							generateExampleFile("examples/data/$moduleKey/$exampleKey"."_source.php",
+//										"examples/$moduleKey/$exampleKey"."_source.html", $useBuildPath);
+							generateExampleFile("examples/module/example.php?name=".urlencode($exampleKey)."&clean=true", 
 										"examples/$moduleKey/$exampleKey"."_source.html", $useBuildPath);
 						}
 					
